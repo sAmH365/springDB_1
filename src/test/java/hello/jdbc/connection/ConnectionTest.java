f25b82dec4a5398e957608c1aa66442857c9d677
sAmH365 <npower5377@naver.com>
2024-09-26 08:37:51 +0900

    datasource exam2 - connectionPool

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -2,6 +2,7 @@ package hello.jdbc.connection;
 
 import static hello.jdbc.connection.ConnectionConst.*;
 
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -30,11 +31,34 @@ public class ConnectionTest {
     useDataSource(datasource);
   }
 
+  @Test
+  void dataSourceConnectionPool() throws SQLException {
+    // 커넥션 풀링
+    HikariDataSource dataSource = new HikariDataSource();
+    dataSource.setJdbcUrl(URL);
+    dataSource.setUsername(USERNAME);
+    dataSource.setPassword(PASSWORD);
+    dataSource.setMaximumPoolSize(10);
+    dataSource.setPoolName("HelloPool");
+
+    useDataSource(dataSource);
+  }
+
   private void useDataSource(DataSource dataSource) throws SQLException {
     Connection con1 = dataSource.getConnection();
     Connection con2 = dataSource.getConnection();
+//    설정한 pool개수 초과해서 생성한 뒤 커넥션이 가득차면 일정시간 기다린뒤(설정에 따라 다름) 예외 발생
+//    Connection con3 = dataSource.getConnection();
+//    Connection con4 = dataSource.getConnection();
+//    Connection con5 = dataSource.getConnection();
+//    Connection con6 = dataSource.getConnection();
+//    Connection con7 = dataSource.getConnection();
+//    Connection con8 = dataSource.getConnection();
+//    Connection con9 = dataSource.getConnection();
+//    Connection con10 = dataSource.getConnection();
+//    Connection con11 = dataSource.getConnection();
 
-    log.info("connection={}, class={}", con1, con1.getClass());
-    log.info("connection={}, class={}", con2, con2.getClass());
+    log.info("connection::{}, class={}", con1, con1.getClass());
+    log.info("connection::{}, class={}", con2, con2.getClass());
   }
 }
